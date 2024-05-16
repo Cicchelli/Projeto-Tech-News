c6ac6cf40997491b1f15e0dbfc5a8ee2d270f69d
Stephan <cicchelli.stephan@gmail.com>
2024-05-16 16:31:35 -0300

    REQ 5

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -2,6 +2,7 @@ import time
 import requests
 from parsel import Selector
 import parsel
+from tech_news.database import create_news
 
 
 # Requisito 1
@@ -59,5 +60,23 @@ def scrape_news(html_content):
 
 # Requisito 5
 def get_tech_news(amount):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    all_news = []
+    url = "https://blog.betrybe.com"
+
+    while len(all_news) < amount:
+        html_content = fetch(url)
+        news_links = scrape_updates(html_content)
+
+        for link in news_links:
+            if len(all_news) < amount:
+                news_content = fetch(link)
+                news_data = scrape_news(news_content)
+                all_news.append(news_data)
+
+        next_page_link = scrape_next_page_link(html_content)
+        if not next_page_link:
+            break
+        url = next_page_link
+
+    create_news(all_news)
+    return all_news
